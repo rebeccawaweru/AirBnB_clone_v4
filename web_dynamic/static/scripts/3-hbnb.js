b008bfb203139bd242731b8b3e99442673212950
rebeccawaweru <wawerur95@gmail.com>
2023-10-07 00:41:00 +0300

    debugging 3-hbnb.html, 3-hbnb.js

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,38 +1,41 @@
-$(document).ready(function () {
-  const amenitiesObj = {};
-  $(document).on('change', "input[type='checkbox']", function () {
-    if (this.checked) {
-      amenitiesObj[$(this).data('id')] = $(this).data('name');
-    } else {
-      delete amenitiesObj[$(this).data('id')];
+$(document).ready(init);
+
+function init () {
+  const amenityObj = {};
+  $('.amenities .popover input').change(function () {
+    if ($(this).is(':checked')) {
+      amenityObj[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else if ($(this).is(':not(:checked)')) {
+      delete amenityObj[$(this).attr('data-name')];
     }
-    const arry = Object.values(amenitiesObj);
-    if (arry.length > 0) {
-      $('div.amenities > h4').text(Object.values(amenitiesObj).join(', '));
-    } else {
-      $('div.amenities > h4').html('&nbsp;');
-    }
+    const names = Object.keys(amenityObj);
+    $('.amenities h4').text(names.sort().join(', '));
   });
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
-    if (textStatus === 'success') {
-      if (data.status === 'OK') {
-        $('#api_status').addClass('available');
-      } else {
-        $('#api_status').removeClass('available');
-      }
-    }
-  });
+  fetchPlaces();
+}
+fetch('http://0.0.0.0:5001/api/v1/status/')
+  .then(response => response.json())
+  .then(data => {
+    const statusDiv = document.querySelector('#api_status');
+     if (data.status === 'OK') {
+       statusDiv.classList.add('available');
+     } else {
+	statusDiv.classList.remove('available');
+     }
+  })
+  .catch(error => console.error('Error:', error));
+function fetchPlaces () {
   $.ajax({
     type: 'POST',
-    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    url: 'http://0.0.0.0:50001/api/v1/places_search',
     data: '{}',
     dataType: 'json',
     contentType: 'application/json',
     success: function (data) {
       for (let i = 0; i < data.length; i++) {
         const place = data[i];
-        $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+	$('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
       }
     }
   });
-});
+}
